a1b9883ba8f7db9f941cdda4d7316559c75550cc
anaaki <naoakitaira@gmail.com>
2018-04-23 19:49:41 +0900

    最初のコミット

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bj',
+    version='0.0.1',
+    description='black jack game',
+    author='naoaki taira',
+    author_email='taira@gmail.com',
+    url='',
+    packages=find_packages(exclude=('tests'))
+)
