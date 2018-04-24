d5980b3994fd784ad25132497279d6fca20c16b4
anaaki <naoakitaira@gmail.com>
2018-04-24 12:21:35 +0900

    deckはSetupに移動。再利用するため

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -4,8 +4,10 @@ import unittest
 
 class TestDeck(unittest.TestCase):
 
+    def setUp(self):
+        self.deck = Deck()
+
     def test_init_deck_state(self):
         """デッキには52枚のカードがありカードの重複が無い
         重複は、コンストラクタでハードコードしている以上自明なので、テストしない"""
-        deck = Deck()
-        self.assertEqual(52, len(deck.cards))
+        self.assertEqual(52, len(self.deck.cards))
