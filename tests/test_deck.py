4f1c122d6f8fc583389ef328f994c4458e09942f
anaaki <naoakitaira@gmail.com>
2018-04-24 12:29:56 +0900

    絵札の扱いをテスト

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -11,3 +11,21 @@ class TestDeck(unittest.TestCase):
         """デッキには52枚のカードがありカードの重複が無い
         重複は、コンストラクタでハードコードしている以上自明なので、テストしない"""
         self.assertEqual(52, len(self.deck.cards))
+
+    def test_jqk_is_10(self):
+        """JとQとKは10として扱う"""
+        jqk_cards = [
+            c for c in self.deck.cards if c.name in ["J", "Q", "K"]]
+
+        self.assertTrue(
+            all([True if c.val == 10 else False for c in jqk_cards])
+        )
+
+    def test_ace_is_10(self):
+        """ Aはとりあえず「1」としてだけ扱う。「11」にはしない"""
+        ace_cards = [
+            c for c in self.deck.cards if c.name in ["A"]]
+
+        self.assertTrue(
+            all([True if c.val == 1 else False for c in ace_cards])
+        )
