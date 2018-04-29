a151fd418ff051b77e07a61ede3ee5bd74f75fce
anaaki <naoakitaira@gmail.com>
2018-04-29 15:14:53 +0900

    判定の実装を追加

--- bj/judge.py
+++ bj/judge.py
@@ -1,9 +1,24 @@
 class Judge:
     def __init__(self):
         self.judge_msg = ""
+        self.BASE = 21
+
     def add_player(self, player, dealer):
         """プレイヤを追加、審判の対象"""
-        pass
-    
+        self.player = player
+        self.dealer = dealer
+
     def final_judge(self):
-        self.judge_msg = "player win" 
+        player_score = sum([card.val
+            for card in self.player.stock
+        ]) - self.BASE
+
+        dealer_score = sum([card.val
+            for card in self.dealer.stock
+        ]) - self.BASE
+
+        if abs(player_score) < abs(dealer_score):
+            self.judge_msg = "player win"
+        else:
+            self.judge_msg = "player lose"
+            
